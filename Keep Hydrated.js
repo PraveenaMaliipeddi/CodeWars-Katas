ab9feaeae915ef8b2e2db424f6746b00e7fef920
PraveenaMaliipeddi <50979392+PraveenaMaliipeddi@users.noreply.github.com>
2021-09-22 06:55:58 -0700

    Create  Keep Hydrated.js

---
+++ Keep Hydrated.js
@@ -0,0 +1,19 @@
+//solution 1:
+
+
+function litres(time) {
+  return Math.floor(time/2);
+
+}
+
+//solution 2:
+
+function litres(time) {
+  return Math.floor(time * 0.5);
+}
+
+//soltion3
+
+function litres(time) {
+  return parseInt(time/2);
+}
